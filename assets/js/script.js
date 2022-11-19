5f5b72070672e1c9afaae7877c76dcbbdfe2cc20
Olubukola <bukyabiona@gmail.com>
2022-11-19 19:38:38 -0400

    the next question is displayed after confirming the selected option to the current question

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,12 +60,25 @@ var timer = document.querySelector(".timer");
 var startButton = document.querySelector("#btn");
 var startBox = document.querySelector(".start-box");
 var quizBox = document.querySelector(".quiz-box");
+var questionBox = document.querySelector(".question-box");
 var timeLeft = 60;
 var loss = 10;
 var createUl = document.createElement("ul");
 var counter = 0;
+var score = 0;
+var questionCount = 1;
+var questionNum = 0;
+var question = document.querySelector(".question");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+var checkAnswer = document.querySelector(".checkAnswer");
+var allOptionButtons = document.querySelectorAll(".option");
 
-startButton.addEventListener("click", function(){
+
+
+function timerCountdown(){
     counter = setInterval(function(){
         timeLeft--;
         timer.textContent = "Time left: " + timeLeft;
@@ -75,8 +88,63 @@ startButton.addEventListener("click", function(){
             timer.textContent = "Time up!";
             
         }
+       
     }, 1000);
-})
+    // displayQuestions(questionNum);
+}
+
+function startQuiz(){
+    quizBox.innerHTML = "";
+    questionBox.style.display = "block";
+    // questionNum = 0;
+    timerCountdown();
+    displayQuestions(questionNum);
+}
+startButton.addEventListener("click", startQuiz);
+
+function displayQuestions(x){
+question.textContent = questions[x].question;
+option1.textContent = questions[x].options[0];
+option2.textContent = questions[x].options[1];
+option3.textContent = questions[x].options[2];
+option4.textContent = questions[x].options[3];
+questionNum = x;
+   
+    console.log("Test");
+
+}
+
+function confirmAnswer(event){
+
+checkAnswer.style.display = "block";
+// console.log(event.target.textContent);
+if(questions[questionNum].answer == event.target.textContent){
+    event.preventDefault();
+    console.log("Correct");
+    checkAnswer.textContent = "Correct!";
+    score += 1;
+}else{
+    timeLeft -= 10;
+    checkAnswer.textContent = "Wrong! The correct answer is " + questions[questionNum].answer + " .";
+}
+setTimeout(function(){
+    checkAnswer.style.display = "none";
+}, 1000);
+
+
+if (questionNum < questions.length){
+    questionNum++;
+    displayQuestions(questionNum);
+}else{
+    gameOver();
+}
+questionCount++;
+}
+
+for (var i=0; i<allOptionButtons.length; i++){
+    allOptionButtons[i].addEventListener("click",confirmAnswer);
+}
+
 function gameOver(){
     quizBox.innerHTML = "";
     timer.innerHTML = "";
